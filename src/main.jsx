eabc29bda83d15e62d525c6cae2f01753e921b4c
Mohammed Issa <126794479+Mohammed-issa0@users.noreply.github.com>
2025-02-04 10:17:22 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import AppRoutes from "./AppRoutes.jsx";
 import ReactPixel from "react-facebook-pixel";
 // إعداد Facebook Pixel
-const pixelId = "0"; // ضع هنا رقم الـ Pixel ID الخاص بك
+const pixelId = "9224381510963175"; // ضع هنا رقم الـ Pixel ID الخاص بك
 ReactPixel.init(pixelId); // تهيئة Facebook Pixel
 ReactPixel.pageView(); // تتبع أول زيارة للصفحة
 
